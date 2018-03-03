5d319b2cad291dff538bb9db2ed76a6c2adb6f22
Camilo Gonzalez <CA@Cashs-MacBook-Pro.local>
2018-03-03 08:13:07 -0500

    Third Commit Game functional still with problems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ Main.WordArray = [];
 Main.WordUArray = [];
 
 Main.Lives = 10;
-Main.NumInWordBank = Words.Length;
+Main.NumInWordBank = Words.length;
 
 Main.Word = "";
 Main.WordU = "";
@@ -12,13 +12,12 @@ Main.WordU = "";
 
 //Function//
 
-Main.Pullword = function() {
-    Main.Word = Words.List[(Math.floor(Math.random()*Main.NumInWordBank))];
-
-}
+Main.PullWord = function() {
+    Main.Word = Words[(Math.floor(Math.random() * Main.NumInWordBank))];
+  }
 
 Main.SetUnderline = function(){
-    Main.Pullword();
+    Main.PullWord();
     for (i=0; i<Main.Word.length; i++){
         Main.WordArray[i] = Main.Word.charAt(i);
         Main.WordUArray[i] = "_";
@@ -64,7 +63,7 @@ Main.UpdateLetter = function(letter){
     }
 }
 
-Main.Pullword();
+Main.PullWord();
 Main.SetUnderline();
 
 
@@ -73,155 +72,181 @@ $(function() {
         event.preventDefault();
         Main.UpdateLetter("a");
     });
-});
+}
+);
 $(function() {
     $("#b").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("b");
     });
-});
+}
+);
 $(function() {
     $("#c").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("c");
     });
-});
+}
+);
 $(function() {
     $("#d").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("d");
     });
-});
+}
+);
 $(function() {
     $("#e").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("e");
     });
-});
+}
+);
 $(function() {
     $("#f").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("f");
     });
-});
+}
+);
 $(function() {
     $("#g").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("g");
     });
-});
+}
+);
 $(function() {
     $("#h").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("h");
     });
-});
+}
+);
 $(function() {
     $("#i").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("i");
     });
-});
+}
+);
 $(function() {
     $("#j").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("j");
     });
-});
+}
+);
 $(function() {
     $("#k").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("k");
     });
-});
+}
+);
 $(function() {
     $("#l").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("l");
     });
-});
+}
+);
 $(function() {
     $("#m").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("m");
     });
-});
+}
+);
 $(function() {
     $("#n").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("n");
     });
-});
+}
+);
 $(function() {
     $("#o").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("o");
     });
-});
+}
+);
 $(function() {
     $("#p").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("p");
     });
-});
+}
+);
 $(function() {
     $("#q").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("q");
     });
-});
+}
+);
 $(function() {
     $("#r").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("r");
     });
-});
+}
+);
 $(function() {
     $("#s").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("s");
     });
-});
+}
+);
 $(function() {
     $("#t").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("t");
     });
-});
+}
+);
 $(function() {
     $("#u").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("u");
     });
-});
+}
+);
 $(function() {
     $("#v").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("v");
     });
-});
+}
+);
 $(function() {
     $("#w").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("w");
     });
-});
+}
+);
 $(function() {
     $("#x").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("x");
     });
-});
+}
+);
 $(function() {
     $("#y").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("y");
     });
-});
+}
+);
 $(function() {
     $("#z").click(function(event){
         event.preventDefault();
         Main.UpdateLetter("z");
 });
-});
+}
+);
 
