9c7ade7ca2ab662c4b190852e018e64531d78335
Camilo Gonzalez <CA@Cashs-MacBook-Pro.local>
2018-03-03 07:34:52 -0500

    Second Commit game not functional yet

--- assets/javascript/words.js
+++ assets/javascript/words.js
@@ -1,17 +1,18 @@
-Words.List[1] = "Cercei";
-Words.List[2] = "Jon Snow";
-Words.List[3] = "Dragon";
-Words.List[4] = "Tyrion";
-Words.List[5] = "Daenerys";
-Words.List[6] = "Khal Drogo";
-Words.List[7] = "Army of the dead";
-Words.List[8] = "Wall";
-Words.List[9] = "King of the North";
-Words.List[10] = "Seven Kingdoms";
-Words.List[11] = "Starks";
-Words.List[12] = "Lannisters";
-Words.List[13] = "Sansa";
-Words.List[14] = "Aria";
-Words.List[15] = "Wildlings";
+var Words = [
+"Cercei",
+"Jon Snow",
+"Dragon",
+"Tyrion",
+"Daenerys",
+"Khal Drogo",
+"Army of the dead",
+"Wall",
+"King of the North",
+"Seven Kingdoms",
+"Starks",
+"Lannisters",
+"Sansa",
+"Aria",
+  "Wildlings" 
+];
 
-Words.Length = Words.List.Length;
