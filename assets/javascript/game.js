a89dede1e1dc0472e45072c6cfe12e1c6a1faa03
Camilo Gonzalez <CA@Cashs-MacBook-Pro.local>
2018-03-02 23:35:51 -0500

    First Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+Main= {};
+Main.WordArray = [];
+Main.WordUArray = [];
+
+Main.Lives = 10;
+Main.NumInWordBank = Words.Length;
+
+Main.Word = "Test";
+Main.WordU = "";
+
+
+
+//Function//
+
+Main.Pullword = function() {
+    Main.Word = Words.List[(Math.floor(Math.random()*Main.NumInWordBank))];
+
+}
+
+Main.SetUnderline = function(){
+    Main.Pullword();
+    for (i=0; i<Main.Word.length; i++){
+        Main.WordArray[i] = Main.Word.charAt(i);
+        Main.WordUArray[i] = "_";
+
+    }
+    Main.WordU = Main.WordUArray.join("");
+    document.getElementById("WORD").innerHTML = Main.WordU;
+    document.getElementById("numLetters").innerHTML = Main.Word.length;
+
+}
+
+Main.UpdateLetter = function(letter){
+    Main.Changes = 0;
+    for(i=0; i<Main.Word.Length; i++){
+        Main.WordArray[i] = Main.Word.charAt(i);
+        if(Main.Word.charAt(i) == letter){
+            Main.WordUArray[i] = letter;
+            Main.Changes += 1;
+        }
+    }
+
+    if(Main.Changes < 1) {
+        Main.Lives -=1;
+        document.getElementById("lives").innerHTML= Main.Lives;
+
+    }
+
+    Main.WordU = Main.WordUArray.join(""); 
+    document.getElementById("WORD").innerHTML = Main.WordU;
+
+    Main.Word1 = Main.WordArray.join("");
+    Main.Word2 = Main.WordUArray.join("");
+
+    if(Main.Word1 == Main.Word2){
+        alert("You Won! Loading A New Word");
+        Window.location.reload();
+    }
+
+    if(Main.Lives < 1){
+        document.getElementById("WORD").innerHTML == Main.Word1;
+        alert("You Have Run Out Of Lives, Please Try Again.");
+        Window.location.reload();
+    }
+}
+
+Main.Pullword();
+Main.SetUnderline();
+
